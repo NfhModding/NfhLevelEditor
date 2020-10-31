4f9ee29f446b3bcb82fec66e67d8d3ec023eaf57
Péter Fónad <fonadpeti98@gmail.com>
2020-10-31 13:08:29 +0100

    Added Application work folder

---
+++ Nfh.Services/ApplicationWorkFolder.cs
@@ -0,0 +1,37 @@
+﻿using Nfh.Domain;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nfh.Services
+{
+    internal class ApplicationWorkFolder : IApplicationWorkFolder
+    {
+        private DirectoryInfo? info;
+        public DirectoryInfo Info
+        {
+            get
+            {
+                if (info == null)
+                {
+                    var appDataFolder = new DirectoryInfo(
+                        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+
+                    var workFolder = new DirectoryInfo(Path.Combine(appDataFolder.FullName, ApplicationInformation.ApplicationName));
+
+                    if (!workFolder.Exists)
+                    {
+                        appDataFolder.Create();
+                    }
+
+                    info = workFolder;
+                }
+
+                return info;
+            }
+        }
+    }
+}
---
+++ Nfh.Services/IApplicationWorkFolder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nfh.Services
+{
+    internal interface IWorkFolder
+    {
+        public DirectoryInfo Info { get; }
+    }
+}
