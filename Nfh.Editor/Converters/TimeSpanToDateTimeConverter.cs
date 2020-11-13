78642f8ae77932f852a0c8e1910b5987a6c8ee77
Lenkefi Péter <lenkefi.peti@gmail.com>
2020-11-13 18:36:37 +0100

    Made view percentage and time limit editable

---
+++ Nfh.Editor/Converters/TimeSpanToDateTimeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Nfh.Editor.Converters
+{
+    public class TimeSpanToDateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return new DateTime();
+            if (value is TimeSpan span) return new DateTime(1, 1, 1, span.Hours, span.Minutes, span.Seconds);
+
+            throw new ArgumentException("Invalid value type!", nameof(value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return new DateTime();
+            if (value is DateTime dt) return dt.TimeOfDay;
+
+            throw new ArgumentException("Invalid value type!", nameof(value));
+        }
+    }
+}
