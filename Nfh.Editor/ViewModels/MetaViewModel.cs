bc30f09cf35a93583438c30dae140c322a0562ab
Lenkefi Péter <lenkefi.peti@gmail.com>
2020-11-13 21:06:16 +0100

    Added modernui icons

---
+++ Nfh.Editor/ViewModels/MetaViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Nfh.Editor.ViewModels
+{
+    public class MetaViewModel
+    {
+        public ICommand New { get; } = ApplicationCommands.New;
+        public ICommand Open { get; } = ApplicationCommands.Open;
+        public ICommand Save { get; } = ApplicationCommands.Save;
+
+        public ICommand Undo { get; } = ApplicationCommands.Undo;
+        public ICommand Redo { get; } = ApplicationCommands.Redo;
+
+        public SeasonPackViewModel SeasonPack { get; }
+
+        public MetaViewModel()
+        {
+            SeasonPack = new SeasonPackViewModel(Services.Project.LoadSeasonPack(Services.GamePath));
+        }
+    }
+}
