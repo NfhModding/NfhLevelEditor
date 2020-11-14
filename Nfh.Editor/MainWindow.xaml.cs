23dd9a2ec05fe7fa0ec3d078a339849a71042a83
Lenkefi Péter <lenkefi.peti@gmail.com>
2020-11-14 19:01:16 +0100

    Update MainWindow.xaml.cs

--- Nfh.Editor/MainWindow.xaml.cs
+++ Nfh.Editor/MainWindow.xaml.cs
@@ -11,11 +11,8 @@ namespace Nfh.Editor
     /// </summary>
     public partial class MainWindow : Window
     {
-        public SeasonPackViewModel SeasonPack { get; set; }
-
         public MainWindow()
         {
-            SeasonPack = new SeasonPackViewModel(Services.Project.LoadSeasonPack(Services.GamePath));
             InitializeComponent();
         }
     }
