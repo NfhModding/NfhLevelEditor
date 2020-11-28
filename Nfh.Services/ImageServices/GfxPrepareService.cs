aeaf9a94e3f3eb25bc5dd2ecf93f0d7a1dfbdeb7
Péter Fónad <fonadpeti98@gmail.com>
2020-11-28 13:49:00 +0100

    Refactor preparing gfx data

--- Nfh.Services/ImageServices/GfxPrepareService.cs
+++ Nfh.Services/ImageServices/GfxPrepareService.cs
@@ -5,9 +5,6 @@ namespace Nfh.Services.ImageServices
 {
     internal class GfxPrepareService : IGfxPrepareService
     {
-        // If null, we need to prepare
-        private DirectoryInfo? gfxDataFolder = null;
-
         private readonly IApplicationWorkFolder applicationWorkFolder;
         private readonly IZipService zipHelper;
 
@@ -17,25 +14,38 @@ namespace Nfh.Services.ImageServices
             this.zipHelper = zipHelper;
         }
 
+        // ToDo make it async
         public DirectoryInfo PrepareGfxData(DirectoryInfo gamePath)
         {
-            if (gfxDataFolder is not null)
-                return gfxDataFolder;
-
             if (!gamePath.Exists)
                 throw new("Game path does not exits");
 
+            var gfxDataFolder = GetGfxDataFolderIfExists(applicationWorkFolder.Info);
+
+            // If the "gfxdata" folder exists we can say the graphics are available -> ToDo what if the user has modified it?
+            if (gfxDataFolder is not null)
+                return gfxDataFolder;
+
+            gfxDataFolder = GetGfxFolderInfo(applicationWorkFolder.Info);
+            gfxDataFolder.Create();
+
             // Unzip gfxdata.bnd to appwork folder
             var gfxdataFile = new FileInfo(Path.Combine(gamePath.FullName, "data", "gfxdata.bnd"));
-
-            var appGfxdataFolder = new DirectoryInfo(Path.Combine(applicationWorkFolder.Info.FullName, "gfxdata"));
-            gfxDataFolder = appGfxdataFolder;
-            if (appGfxdataFolder.Exists) return gfxDataFolder;
-            
-            appGfxdataFolder.Create();
-            zipHelper.UnzipToFolderWithOverride(gfxdataFile, appGfxdataFolder);
+            zipHelper.UnzipToFolderWithOverride(gfxdataFile, gfxDataFolder);
 
             return gfxDataFolder;
         }
+
+        private static DirectoryInfo? GetGfxDataFolderIfExists(DirectoryInfo workingFolder)
+        {
+            var appGfxdataFolder = GetGfxFolderInfo(workingFolder);
+            if (appGfxdataFolder.Exists)
+                return appGfxdataFolder;
+
+            return null;
+        }
+
+        private static DirectoryInfo GetGfxFolderInfo(DirectoryInfo workingFolder) =>
+            new DirectoryInfo(Path.Combine(workingFolder.FullName, "gfxdata"));
     }
 }
